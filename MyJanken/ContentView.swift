98457bd3de75af838c40d0e2e720a4787ec4f40a
Hayato <hayato.314.nico@gmail.com>
2023-12-23 02:26:11 +0900

    レイアウト調整

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -11,24 +11,33 @@ struct ContentView: View {
     @State var jankenNumber = 0
     var body: some View {
         VStack {
+            //画面下部いっぱいまで広げる
+            Spacer()
             if jankenNumber == 0{
                 Text("これからじゃんけんをします")
+                    .padding(.bottom)
             }else if jankenNumber == 1{
 //                Image(.gu)　だとエラー
                 Image("gu")
                     .resizable()
                     .scaledToFit()
+                Spacer()
                 Text("グー")
+                    .padding(.bottom)
             }else if jankenNumber == 2{
                 Image("choki")
                     .resizable()
                     .scaledToFit()
+                Spacer()
                 Text("チョキ")
+                    .padding(.bottom)
             }else {
                 Image("pa")
                     .resizable()
                     .scaledToFit()
+                Spacer()
                 Text("パー")
+                    .padding(.bottom)
             }
             Button(action: {
 //                    print("タップされたよ")
@@ -42,6 +51,11 @@ struct ContentView: View {
                 jankenNumber = newJankenNumber
             }, label: {
                 Text("じゃんけんをする")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 100)
+                    .font(.title)
+                    .background(.pink)
+                    .foregroundColor(.white)
             })
         }
     }
