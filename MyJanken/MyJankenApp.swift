cc381e3c6c786a15ae48d0059ffeb06b8bdfc880
Hayato <hayato.314.nico@gmail.com>
2023-12-23 00:41:10 +0900

    Initial Commit

---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by Yamamoto Hayato on 2023/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
